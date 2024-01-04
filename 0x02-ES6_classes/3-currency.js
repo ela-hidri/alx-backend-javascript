29f890a0f347717ec3b9b56978f0d21c4104a5c3
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-04 22:24:09 +0100

    adding 3-currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,3 +1,32 @@
-class Currency{
-  constructor(code, )
+export default class Currency {
+  constructor(code, name) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get code() {
+    return this._name;
+  }
+
+  set code(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
