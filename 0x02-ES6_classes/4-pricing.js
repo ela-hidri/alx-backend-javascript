e0bb1f2bbeb310f8921de95076e15d8e02167f79
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-04 22:40:46 +0100

    adding 4-pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof (value) !== 'number') {
+      throw new TypeError('amount must be a string');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) { this._currency = value; } else {
+      throw new TypeError('currency  must be a Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
