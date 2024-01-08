4c46e869e6b656cb1fc3a9de87a109ba4cb7f41c
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-08 21:47:47 +0100

    fixing eslint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,19 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   let rst = [];
-  rst = students.filter((student) => student.location === city)
+  rst = students.filter((student) => student.location === city);
   rst = rst.map((student) => {
     const newgrade = newGrades.filter((grade) => grade.studentId === student.id);
-    if (newgrade.length > 0) 
-	  return (
-		  { ...student,
-			  grade: newgrade[0].grade});
+    if (newgrade.length > 0) {
+      return ({
+        ...student,
+        grade: newgrade[0].grade,
+      });
+    }
     return (
-                  { ...student,
-                          grade: 'N/A'});
+      {
+        ...student,
+        grade: 'N/A',
+      });
   });
   return (rst);
 }
