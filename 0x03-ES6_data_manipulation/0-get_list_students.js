a495bc36ff2e1893a2706be8233f9f9b826e7c8d
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-08 19:52:42 +0100

    adding 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const tab = [];
+  tab.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  tab.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  tab.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+}
