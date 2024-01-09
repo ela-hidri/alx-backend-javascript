436278219e12870cf6a6c58cf530e0da9ce2394d
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-09 20:12:36 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const z = new Int8Array(buffer, position, value);
-  return z;
+  const z = new Int8Array(buffer);
+  if (position > z.length) throw new Error('Position outside range');
+  z[position] = value;
+  return new DataView(buffer);
 }
