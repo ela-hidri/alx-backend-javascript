3e02a12baec1ed8cdc65dcdcc2300e6a623bef18
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-09 21:24:07 +0100

    adding 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString && startString.lenght > 0)) {
+      if (str === '') { str += value.replace(startString, ''); } else { str += `-${value.replace(startString, '')}`; }
+    }
+  });
+  return str;
+}
