9631879077d7a84d69d6d59850f775ba7d9cca15
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-09 22:10:56 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0 || typeof startString !== 'string') { return ''; }
   set.forEach((value) => {
     if (value.startsWith(startString) && typeof value === 'string') {
-      if (str === '') { str = value.replace(startString, ''); } else { str += `-${value.replace(startString, '')}`; }
+      if (str === '') { str = value.slice(startString.length); } else { str += `-${value.slice(startString.length)}`; }
     }
   });
   return str;
