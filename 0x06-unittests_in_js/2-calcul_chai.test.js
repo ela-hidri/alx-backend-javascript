34f3d4da7c695cdbb6c469976c7e2044b87bbb4d
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-28 21:19:11 +0100

    fixing file

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,21 +1,7 @@
 expect = require('chai').expect;
 const calculateNumber = require("./1-calcul.js");
 
-describe('calculateNumber function', function() {
-    it('should return the sum of two numbers', function(){
-        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    })
-
-    it('should return the division of two numbers', function(){
-        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    })
-
-    it('should return the sub of two numbers', function(){
-        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    })
-
-    it('should return Error if division by 0', function(){
-        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-    })
-
-})
+expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
