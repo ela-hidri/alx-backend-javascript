f59bb4a8651a19d2826186e9193cb0c112de59a4
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-28 21:55:05 +0100

    adding 3-payment

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,26 @@
+const Utils = {
+    calculateNumber: function(type, a, b) {
+    let rst = 0;
+    switch (type) {
+        case 'SUM':
+            rst = Math.round(a) + Math.round(b)
+            break;
+        case 'SUBTRACT':
+            rst = Math.round(a) - Math.round(b)
+            break;
+        case 'DIVIDE':
+            if (Math.round(b) == 0){
+                return 'Error';
+            }
+            rst = Math.round(a) / Math.round(b)
+            break;
+    
+        default:
+            break;
+    }
+    
+    return rst;
+    }
+}
+
+module.exports = Utils
