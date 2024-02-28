164f1f92954b53b9cb33eadeff5c3dbbbe332792
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-28 21:07:58 +0100

    adding chai test

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,21 @@
+const  expect = require('chai');
+const calculateNumber = require("./1-calcul.js");
+
+describe('calculateNumber function', function() {
+    it('should return the sum of two numbers', function(){
+        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    })
+
+    it('should return the division of two numbers', function(){
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    })
+
+    it('should return the sub of two numbers', function(){
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    })
+
+    it('should return Error if division by 0', function(){
+        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    })
+
+})
