7c5c8871d90542394f3483aaf6ec244b567d5706
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-28 21:22:05 +0100

    adding changes

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,6 +2,8 @@ expect = require('chai').expect;
 const calculateNumber = require("./1-calcul.js");
 
 expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
 expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
 expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+expect(calculateNumber('DIVIDE', 1.4, 0.2)).to.equal('Error');
+expect(calculateNumber('SUBTRACT', 4.6, 1.7)).to.equal(3);
