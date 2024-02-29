9c21f74a6eca2cdd43493db762ce314aa0687543
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-29 21:08:22 +0100

    fixing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,29 +1,36 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Api suite', () => {
-  it('should respond with Welcome to the payment system', (done) => {
+describe('API suite', () => {
+  const API_URL = 'http://localhost:7865';
 
-    request('http://localhost:7865/', (error, response, body) => {
-      expect(response.statusCode).to.be.equal(200);
+  it('Gshoud respond correct', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
-
-    it('GET /cart/:id returns correct response for valid :id', (done) => {
-      request.get(`${API_URL}/cart/2`, (_err, res, body) => {
-        expect(res.statusCode).to.be.equal(200);
-        expect(body).to.be.equal('Payment methods for cart 2');
-        done();
-      });
-    });
-  
-    it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
-      request.get(`${API_URL}/cart/hohoho`, (_err, res, _body) => {
-        expect(res.statusCode).to.be.equal(404);
-        done();
-      });
-
   });
-});
+
+  it('should respond for correct id type', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
+    });
+  });
+
+  it('should respond for negative id', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('should respond for wrong id type', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
 });
