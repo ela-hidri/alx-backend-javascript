bc3347e17863bed54043c9bd82d786579889cff7
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-29 21:40:41 +0100

    fixing file

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT =  7865
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system')
 })
@@ -17,13 +19,11 @@ app.get('/available_payments', (_req, res) => {
 
 app.post('/login', (req, res) => {
   let username = '';
-
   if (req.body) {
     username = req.body.userName;
   }
-
   res.send(`Welcome ${username}`);
-});
+});;
 
 app.listen(PORT, () => {
     console.log('API available on localhost port 7865')
